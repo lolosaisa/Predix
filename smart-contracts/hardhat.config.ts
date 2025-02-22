a1e41db1c075d0f932df19f8aa4ffdfedcad857e
lolosaisa <lwamaitha3924@gmail.com>
2025-02-22 13:38:18 +0300

    configured hardhat with lisk

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,8 +1,18 @@
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require('dotenv').config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    lisk: {
+      url: 'https://rpc.sepolia-api.lisk.com',
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+  },
 };
 
 export default config;
