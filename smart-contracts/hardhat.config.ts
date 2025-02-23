991835b76cbe3323149535ea6568264365dc2217
lolosaisa <lwamaitha3924@gmail.com>
2025-02-23 19:46:11 +0300

    upgraded the hardhat config to handle lisk sepolia

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -7,7 +7,12 @@ require('dotenv').config();
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    lisk: {
+    sepolia: {
+      url: `https://eth-sepolia.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.WALLET_KEY}`],
+    },
+    "lisk-sepolia": {
+
       url: 'https://rpc.sepolia-api.lisk.com',
       accounts: [process.env.WALLET_KEY as string],
       gasPrice: 1000000000,
